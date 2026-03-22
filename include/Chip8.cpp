91399911a576cf0a6e59bcf160a4975add878bc3
Ali Edrees <ali.edrees@campus.technion.ac.il>
2026-03-22 20:39:43 +0200

    checpoint mid implementing instructions and cycle method

--- include/Chip8.cpp
+++ include/Chip8.cpp
@@ -2,7 +2,22 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <random>
 using std::ifstream;
+
+
+uint8_t& Chip8::GetVX(){
+    return V[(ir & 0x0F00) >> 8];
+}
+const uint8_t& Chip8::GetVX() const {
+    return V[(ir & 0x0F00) >> 8];
+}
+uint8_t& Chip8::GetVY(){
+    return V[(ir & 0x00F0) >> 4];
+}
+const uint8_t& Chip8::GetVY() const {
+    return V[(ir & 0x00F0) >> 4];
+}
 
 // font array
 static constexpr unsigned int FONTSET_SIZE = 80;
@@ -28,21 +43,221 @@ static const uint8_t FONTSET[FONTSET_SIZE] = {
 
 // c'tor
 Chip8::Chip8()
-    : pc(START_OF_PROGRAM),
+    : generator(std::random_device{}()),
+      dist(0, 255),
+      memory{},
+      V{},
       I(0),
+      pc(START_OF_PROGRAM),
       sp(0),
+      stack{},
       dt(0),
       st(0),
-      memory{},
-      stack{},
-      V{},
+      shouldRender(false),
       display{},
       keyboard{}
 {
     for (int i = 0; i < FONTSET_SIZE; i++){
         memory[i] = FONTSET[i];
     }
+    std::random_device rd;
+    generator.seed(rd());
 }
+
+// chip8 instructions
+
+// CLS (clear display)
+void Chip8::IN_00E0(){
+    for (int i = 0 ; i < DISPLAY_HEIGHT * DISPLAY_WIDTH; i++){
+        display[i] = 0;
+    }
+    shouldRender = true;
+    pc += 2;
+}
+
+// RET (return from a subroutine)
+void Chip8::IN_00EE(){
+    pc = stack[sp--];
+}
+
+// JP addr (jump to address NNN)
+void Chip8::IN_1NNN(){
+    pc = ir & 0xFFF;
+}
+
+// CALL addr (call subroutine at NNN)
+void Chip8::IN_2NNN(){
+    if (sp >= F){
+        std::cerr << "ERROR: Stack Overflow! Too many nested calls." << std::endl;
+    }
+    stack[++sp] = pc;
+    pc += 2;
+}
+
+// SE VX, byte (skip next instruction if VX == KK)
+void Chip8::IN_3XKK(){
+    uint8_t vx = GetVX();
+    uint8_t byte = ir & 0x00FF;
+    
+    if (vx == byte){
+        pc += 4;
+    } else {
+        pc += 2;
+    }
+}
+
+// SNE VX, byte (skip next instruction if VX != KK)
+void Chip8::IN_4XKK(){
+    uint8_t vx = GetVX();
+    uint8_t byte = ir & 0x00FF;
+    
+    if (vx != byte){
+        pc += 4;
+    } else {
+        pc += 2;
+    }
+}
+
+// SE VX, VY (skip next instruction if VX == VY)
+void Chip8::IN_5XY0(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+
+    if (vx == vy){
+        pc += 4;
+    } else {
+        pc += 2;
+    }
+}
+
+// LD VX, byte (set vx = kk)
+void Chip8::IN_6XKK(){
+    uint8_t vx = GetVX();
+    vx = ir & 0x00FF;
+    pc += 2;
+}
+
+// ADD VX, byte
+void Chip8::IN_7XKK(){
+    uint8_t vx = GetVX();
+    vx += ir & 0x00FF;
+    pc += 2;
+}
+
+// LD VX, VY (vx = vy)
+void Chip8::IN_8XY0(){
+    uint8_t vy = GetVY();
+    uint8_t vx = GetVX();
+    vx = vy;
+    pc += 2;
+}
+
+// OR VX, VY (vx = vx | vy)
+void Chip8::IN_8XY1(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+    vx = vx | vy;
+    pc += 2;
+}
+
+// AND VX, VY
+void Chip8::IN_8XY2(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+    vx = vx & vy;
+    pc += 2;
+}
+
+// XOR VX, VY
+void Chip8::IN_8XY3(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+    vx = vx ^ vy;
+    pc += 2;
+}
+
+// ADD VX, VY
+void Chip8::IN_8XY4(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+    int res = vx + vy;
+    vy = (res > 255) ? 1 : 0;
+    vx = res & 0xFF;
+    pc += 2;
+}
+
+// SUB VX, VY
+void Chip8::IN_8XY5(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+    
+    V[F] = (vx > vy) ? 1 : 0;
+    vx = vx - vy;
+    pc += 2;
+}
+
+// SHR VX, {, Vy} (SRL)
+void Chip8::IN_8XY6(){
+    uint8_t vx = GetVX();
+    
+    V[F] = vx & 0x1;
+    vx >>=1;
+    pc += 2;
+}
+
+// SUBN VX, VY
+void Chip8::IN_8XY7(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+    
+    V[F] = (vx < vy) ? 1 : 0;
+    vx = vy - vx;
+    pc += 2;
+}
+
+// SHL VX, {, VY}
+void Chip8::IN_8XYE(){
+    uint8_t vx = GetVX();
+    V[F] = (vx >> 7) ? 1 : 0;
+    vx <<= 2;
+    pc += 2;
+}
+
+// SNE VX, VY (skip next inst if vx != vy)
+void Chip8::IN_9XY0(){
+    uint8_t vx = GetVX();
+    uint8_t vy = GetVY();
+
+    if (vx != vy){
+        pc += 4;
+    } else {
+        pc += 2;
+    }
+}
+
+// LD I, addr
+void Chip8::IN_ANNN(){
+    I = ir & 0x0FFF;
+    pc += 2;
+}
+
+// JP V0, addr (jump to v0 + nnn)
+void  Chip8::IN_BNNN(){
+    pc = V[0] + (ir & 0xFFF);
+}
+
+// RND VX, byte 
+void Chip8::IN_CXKK(){
+    uint8_t vx = GetVX();
+    uint8_t kk = ir & 0xFF;
+    uint8_t rnd = dist(generator);
+    vx = rnd & kk;
+    pc += 2;
+}
+
+// DRW VX, VY, nibble
+
+
 
 // loadROM
 void Chip8::LoadROM(const string& path){
@@ -72,3 +287,27 @@ void Chip8::LoadROM(const string& path){
     }
 }
 
+// Cycle
+void Chip8::Cycle(){
+    // fetch
+    ir = (memory[pc] << 8) | memory[pc + 1];
+    uint16_t opcode = ir && 0xF000;
+    // decode
+    switch (opcode){
+        case 0x0000:
+            switch (ir){
+                case 0x00E0: //CLS
+                    
+            }
+        case 0x1000: // jump
+            pc = ir && 0xFFF;
+        case 0x6000: // Vx = nn
+            V[(ir & 0x0F00) >> 8] = ir & 0x00FF;
+            pc += 2;
+        case 0x7000: // Vx += nn
+            V[(ir & 0x0F00) >> 8] += ir & 0x00FF;
+            pc += 2;
+        
+    }
+
+}
--- include/Chip8.h
+++ include/Chip8.h
@@ -7,25 +7,82 @@ using std::string;
 class Chip8 {
 private:
     static constexpr int MEMORY_SIZE = 4096;
-    static constexpr int F = 16;
+    static constexpr int F = 15;
     static constexpr int START_OF_PROGRAM = 0x200;
+    static constexpr int DISPLAY_WIDTH = 64;
+    static constexpr int DISPLAY_HEIGHT = 32;
+
+    void IN_00E0();
+    void IN_00EE();
+    void IN_1NNN();
+    void IN_2NNN();
+    void IN_3XKK();
+    void IN_4XKK();
+    void IN_5XY0();
+    void IN_6XKK();
+    void IN_7XKK();
+    void IN_8XY0();
+    void IN_8XY1();
+    void IN_8XY2();
+    void IN_8XY3();
+    void IN_8XY4();
+    void IN_8XY5();
+    void IN_8XY6();
+    void IN_8XY7();
+    void IN_8XYE();
+    void IN_9XY0();
+    void IN_ANNN();
+    void IN_BNNN();
+    void IN_CXKK();
+    void IN_DXYN();
+    void IN_EX9E();
+    void IN_EXA1();
+    void IN_FX07();
+    void IN_FX0A();
+    void IN_FX15();
+    void IN_FX18();
+    void IN_FX1E();
+    void IN_FX29();
+    void IN_FX33();
+    void IN_FX65();
+
+    uint8_t& GetVX();
+    const uint8_t& Chip8::GetVX() const; 
+    uint8_t& Chip8::GetVY();
+    const uint8_t& Chip8::GetVY() const;
+
+    // random generator
+    std::mt19937 generator;
+    std::uniform_int_distribution<uint8_t> dist;
 public:
     uint8_t memory[MEMORY_SIZE]; //4KB RAM
-    uint8_t V[F]; // 16 registers (V0-VF)
+    uint8_t V[F+1]; // 16 registers (V0-VF)
     uint16_t I; // index register (addresses)
     uint16_t pc; // program counter 
-
+    uint16_t ir; // usually called IR register holds the current instruction
     //stack
     uint8_t sp; // stack pointer
-    uint16_t stack[F]; //stack size 16
+    uint16_t stack[F+1]; //stack size 16
 
     //delay & timer
     uint8_t dt; // delay timer
     uint8_t st; // sound timer
 
-    uint8_t display[64 * 32]; // display in pixles
-    uint8_t keyboard[F]; // keyboard 0-F (in hex)
+    bool shouldRender;
+    uint8_t display[DISPLAY_HEIGHT * DISPLAY_WIDTH]; // display in pixles
+    uint8_t keyboard[F+1]; // keyboard 0-F (in hex)
 
     Chip8(); // c'tor
     void LoadROM(const string& path); 
+    void Cycle(); // fetch decode execute
 };
+/* display
+(0,0)                                              (63,0)
+ ................................................................
+ ..........................########..............................
+ ..........................#......#..............................
+ ..........................#......#..............................
+ ..........................########..............................
+ ................................................................
+(0,31)                                             (63,31)
+*/
