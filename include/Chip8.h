f3f924009481a279d4c01888ce832d81583e04c3
Ali Edrees <ali.edrees@campus.technion.ac.il>
2026-03-22 15:40:00 +0200

    change name to match class name

---
+++ include/Chip8.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdint> // to access 16/8 bit types
+
+class Chip8 {
+private:
+    static constexpr int MEMORY_SIZE = 4096;
+    static constexpr int F = 16;
+public:
+    uint8_t memory[MEMORY_SIZE]; //4KB RAM
+    uint8_t V[F]; // 16 registers (V0-VF)
+    uint16_t I; // index register (addresses)
+    uint16_t pc; // program counter 
+
+    //stack
+    uint8_t sp; // stack pointer
+    uint16_t stack[F]; //stack size 16
+
+    //delay & timer
+    uint8_t dt; // delay timer
+    uint8_t st; // sound timer
+
+    uint8_t display[64 * 32]; // display in pixles
+    uint8_t keyboard[F]; // keyboard 0-F (in hex)
+
+    Chip8(); // c'tor
+};
